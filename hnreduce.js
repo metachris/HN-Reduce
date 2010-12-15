e9d12609db7f7c58a9f371fb7a89a8ef50db21da
Chris Hager <chager@myyearbook.com>
2010-12-14 23:06:34 -0500

    localStorage in background.html works

--- hnreduce.js
+++ hnreduce.js
@@ -1,34 +1,17 @@
-function getItemRows() {
-    // Get the main table container
-    rows1 = document.getElementsByTagName("tr");
-    if (rows1.length < 3) return false;
-    
-    // Get the tr from the main container which has the actual item table inside
-    container = rows1[3]; // <tr> with the table with all news items
-    
-    // Get all the item tr's
-    items = container.getElementsByTagName("tr");    
-    // items[0] ... first <tr> with item number, votearrow, title, url
-    // items[1] ... first <tr> with metainfo
-    // items[2] ... first <tr> with blank line
-    
-    return items;
-}
-
 function remove(id, href) { 
-    //for (var x in localStorage) {
-    //    console.log("- " + x);
-    //}
-    console.log("remove: " + id + " > " + href);
     localStorage[href] = "1";
     _remove(href);
+
+    // Push info to main extension so we can show it in the options later on
+    chrome.extension.sendRequest({remove: href}, function(response) {
+        console.log(response.farewell);
+    });
 }
 
 function _remove(href) {
-    console.log("x " + href);
+    // console.log("x " + href);
     rows1 = document.getElementsByTagName("tr");
     tbody = rows1[3].getElementsByTagName("tbody")[0];
-
     rows = rows1[3].getElementsByTagName("tr");
 
     var len = rows.length;
@@ -38,9 +21,7 @@ function _remove(href) {
             if (cols.length > 2) {
                 td_title = cols[2];                
                 link = td_title.getElementsByTagName("a")[0];
-                //console.log(link.href);
                 if (link.href == href) {
-                    //console.log("found");
                     // found   
                     i1 = rows[i];
                     i2 = rows[i+1];
@@ -55,12 +36,28 @@ function _remove(href) {
     }            
 }
     
-function checkItemRows(rows) {
-    if (rows === false) return false;
-    //var id = 0;
-
+function checkItemRows() {
     rows1 = document.getElementsByTagName("tr");
-    tbody = rows1[3].getElementsByTagName("tbody")[0];
+    if (rows1.length < 3) return false;
+
+    var port = chrome.extension.connect({name: "isremoved"});
+    port.postMessage({href: "Knock knock"});
+    port.onMessage.addListener(function(msg) {
+        //console.log("is removed: " + msg.removed);
+        if (msg.removed) {
+            _remove(msg.removed);   
+        }
+    });
+    
+    // rows[0] ... first <tr> with item number, votearrow, title, url
+    // rows[1] ... first <tr> with metainfo
+    // rows[2] ... first <tr> with blank line
+    
+    // Get the tr from the main container which has the actual item table inside
+    container = rows1[3]; // <tr> with the table with all news items
+    
+    // Get all the item tr's
+    rows = container.getElementsByTagName("tr");    
 
     var toRemove = Array();
         
@@ -75,37 +72,21 @@ function checkItemRows(rows) {
                 link = td_title.getElementsByTagName("a")[0];
                 // console.log(link[0].href);
                 
-                // if link is found, remove this and next two rows from DOM
-                // else add remove tag
-                removeCurrentItem = false;
-                if (localStorage[link.href] == "1") {
-                    removeCurrentItem = true;
-                    toRemove.push(link.href);
-                }
-                //console.log(link[0]);
-                
-                if (!removeCurrentItem) {
-                    // Add remove column
-                    x = document.createElement('td');
-                    x.innerHTML = " <small><a href='javascript:void(0);' style='color:lightgray;' onmouseover='this.parentNode.parentNode.parentNode.style.background=\"#fbfbf9\"' onmouseout='this.parentNode.parentNode.parentNode.style.background=\"\"'>[remove]</a></small>";
-                    x.reduce_id = i;
-                    x.reduce_href = link.href; 
-                    x.addEventListener("click", function(e) {
-                        remove(this.reduce_id, this.reduce_href);
-                    }, false);
-                    rows[i].appendChild(x);
-                }
-                
-                //id += 1;
-                //console.log(id);
+                // Add remove column
+                x = document.createElement('td');
+                x.innerHTML = " <small><a href='javascript:void(0);' style='color:lightgray;' onmouseover='this.parentNode.parentNode.parentNode.style.background=\"#fbfbf9\"' onmouseout='this.parentNode.parentNode.parentNode.style.background=\"\"'>[remove]</a></small>";
+                x.reduce_id = i;
+                x.reduce_href = link.href; 
+                x.addEventListener("click", function(e) {
+                    remove(this.reduce_id, this.reduce_href);
+                }, false);
+                rows[i].appendChild(x);
+
+                // Ask the main extension if this url is removed
+                port.postMessage({href: link.href});                
             }
         }
     }
-    
-    for (var i in toRemove) {
-        _remove(toRemove[i]);
-    }
 }
 
-rows = getItemRows();
-checkItemRows(rows);
+checkItemRows();
